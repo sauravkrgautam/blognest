1f28384cd233e52dc813a1171c63371726af6fbe
sauravkrgautam <sauravgautam2419@gmail.com>
2025-07-15 16:17:01 +0530

    Fix: Correct Cloudinary environment variable keys

--- BlogNest/settings.py
+++ BlogNest/settings.py
@@ -144,6 +144,12 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media files storage
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
